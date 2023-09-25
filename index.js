957d1d51983abf5b6e726563d85b6e21a4b1351e
Olojakpoke Daniel <holodja@hotmail.com>
2023-09-25 02:50:38 +0100

    fix: update dist/index.js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-import core from '@actions/core';
-import * as github from '@actions/github'
-import GitHubProject from 'github-project';
+import core from "@actions/core";
+import * as github from "@actions/github";
+import GitHubProject from "github-project";
 
 const run = async () => {
   try {
-    const owner = core.getInput('owner');
-    const number = Number(core.getInput('number'));
-    const token = core.getInput('token');
-    const iterationField = core.getInput('iteration-field'); // name of the iteration field
-    const newiterationType = core.getInput('new-iteration'); // current or next
-    const repoToken = core.getInput('repo-token')
+    const owner = core.getInput("owner");
+    const number = Number(core.getInput("number"));
+    const token = core.getInput("token");
+    const iterationField = core.getInput("iteration-field"); // name of the iteration field
+    const newiterationType = core.getInput("new-iteration"); // current or next
 
-    const client = github.getOctokit(repoToken)
-    const { pull_request: event } = github.context.payload
-    const { node_id } = event
-    core.info(node_id)
-    const project = new GitHubProject({ owner, number, token, fields: { iteration: iterationField } });
+    const { pull_request: event } = github.context.payload;
+    const { node_id } = event;
+    core.info(node_id);
+    const project = new GitHubProject({
+      owner,
+      number,
+      token,
+      fields: { iteration: iterationField },
+    });
 
     const projectData = await project.getProperties();
 
-    const currentIteration = projectData.fields.iteration.configuration.iterations[0];
-    const nextIteration = projectData.fields.iteration.configuration.iterations[1];
+    const currentIteration =
+      projectData.fields.iteration.configuration.iterations[0];
+    const nextIteration =
+      projectData.fields.iteration.configuration.iterations[1];
 
-    const newIteration = newiterationType === 'current' ? currentIteration : nextIteration;
+    const newIteration =
+      newiterationType === "current" ? currentIteration : nextIteration;
 
     const items = await project.items.list();
     await project.items.add(node_id, { iteration: newIteration.title });
