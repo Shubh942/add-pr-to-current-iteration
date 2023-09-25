16af6b47a43f5a3b4623cf9847a86598ee4fa32c
Olojakpoke Daniel <holodja@hotmail.com>
2023-09-25 02:38:27 +0100

    feat: add project to iteration

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import core from '@actions/core';
+import * as github from '@actions/github'
 import GitHubProject from 'github-project';
 
 const run = async () => {
@@ -7,26 +8,24 @@ const run = async () => {
     const number = Number(core.getInput('number'));
     const token = core.getInput('token');
     const iterationField = core.getInput('iteration-field'); // name of the iteration field
-    const iterationType = core.getInput('iteration'); // last or current
     const newiterationType = core.getInput('new-iteration'); // current or next
-    const statuses = core.getInput('statuses').split(',');
+    const repoToken = core.getInput('repo-token')
 
+    const client = github.getOctokit(repoToken)
+    const { pull_request: event } = github.context.payload
+    const { node_id } = event
+    core.info(node_id)
     const project = new GitHubProject({ owner, number, token, fields: { iteration: iterationField } });
 
     const projectData = await project.getProperties();
 
-    const lastIteration = projectData.fields.iteration.configuration.completedIterations[0];
     const currentIteration = projectData.fields.iteration.configuration.iterations[0];
     const nextIteration = projectData.fields.iteration.configuration.iterations[1];
 
-    const iteration = iterationType === 'last' ? lastIteration : currentIteration;
     const newIteration = newiterationType === 'current' ? currentIteration : nextIteration;
 
     const items = await project.items.list();
-
-    const filteredItems = items.filter(item => statuses.includes(item.fields.status) && item.fields.iteration === iteration.title);
-
-    await Promise.all(filteredItems.map(item => project.items.update(item.id, { iteration: newIteration.title })));
+    await project.items.add(node_id, { iteration: newIteration.title });
   } catch (error) {
     core.setFailed(error.message);
   }
